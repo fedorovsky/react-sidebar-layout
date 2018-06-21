eb595d6c9ce6a953abe53323d99b26f73e35de60
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2018-06-21 18:37:28 +0300

    add swipe

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 import { bool, func } from 'prop-types';
 
-const Overlay = (props, context) => (
-    // eslint-disable-next-line
-    <div
-      className={classNames(context.setClassWithTheme('rts-overlay'))}
-      style={{
-        opacity: context.isOpenSidebar ? '1' : '0',
-        visibility: context.isOpenSidebar ? 'visible' : 'hidden',
-      }}
-      onClick={context.closeSidebar}
-    />
-);
-Overlay.propTypes = {};
-Overlay.defaultProps = {};
-Overlay.contextTypes = {
-  setClassWithTheme: func,
-  openSidebar: func,
-  closeSidebar: func,
-  isOpenSidebar: bool,
-};
+class Overlay extends Component {
+  static propTypes = {};
+  static defaultProps = {};
+  static contextTypes = {
+    setClassWithTheme: func,
+    closeSidebar: func,
+    isOpenSidebar: bool,
+  };
+
+  state = {};
+
+  move = {
+    startX: 0,
+    finishX: 0,
+  };
+
+  handleSwipeStart = (event) => {
+    this.move.startX = event.touches[0].clientX;
+  };
+
+  handleSwipeEnd = (event) => {
+    this.move.finishX = event.changedTouches[0].clientX;
+    this.swipeEnd(this.move);
+  };
+
+  swipeEnd = ({ startX, finishX }) => {
+    if ((startX - finishX) > 100) {
+      this.context.closeSidebar();
+    }
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      // eslint-disable-next-line
+      <div
+        className={classNames(this.context.setClassWithTheme('rts-overlay'))}
+        style={{
+          opacity: this.context.isOpenSidebar ? '1' : '0',
+          visibility: this.context.isOpenSidebar ? 'visible' : 'hidden',
+        }}
+        onClick={this.context.closeSidebar}
+        onTouchStart={this.handleSwipeStart}
+        onTouchEnd={this.handleSwipeEnd}
+      />
+    );
+  }
+}
 
 export default Overlay;
