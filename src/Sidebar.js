88f68e4d3e157a59642c4bd70ae57c7711e1f631
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2018-06-27 10:09:32 +0300

    remove old context

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,25 +3,22 @@ import classNames from 'classnames';
 import { node } from 'prop-types';
 import SidebarContext from './SidebarContext';
 
-const Sidebar = ({ children, ...other }, context) => {
-  console.log('Sidebar', other, context);
-  return (
-    <SidebarContext.Consumer>
-      {({ setClassWithTheme, isOpenSidebar, openSidebar, closeSidebar }) => (
-        <div
-          className={classNames(setClassWithTheme('rts-sidebar'))}
-          style={{
-            transform: isOpenSidebar ? 'translateX(0%)' : 'translateX(-100%)',
-          }}
-        >
-          <button onClick={openSidebar}>Open</button>
-          <button onClick={closeSidebar}>Close</button>
-          {children}
-        </div>
-      )}
-    </SidebarContext.Consumer>
-  );
-};
+const Sidebar = ({ children }) => (
+  <SidebarContext.Consumer>
+    {({ setClassWithTheme, isOpenSidebar, openSidebar, closeSidebar }) => (
+      <div
+        className={classNames(setClassWithTheme('rts-sidebar'))}
+        style={{
+          transform: isOpenSidebar ? 'translateX(0%)' : 'translateX(-100%)',
+        }}
+      >
+        <button onClick={openSidebar}>Open</button>
+        <button onClick={closeSidebar}>Close</button>
+        {children}
+      </div>
+    )}
+  </SidebarContext.Consumer>
+);
 
 Sidebar.propTypes = {
   children: node.isRequired,
